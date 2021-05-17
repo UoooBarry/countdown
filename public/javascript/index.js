9af7039851748d333d1735510853d7047acacef0
Barry Huang <barry.h@shopperplus.com>
2021-05-17 16:49:38 +0800

    Add motivational quoto

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,3 +6,17 @@ setInterval(() => {
   const timeDif = dif._data;
   document.querySelector('#counter').innerHTML = `${timeDif.days}天${timeDif.hours}时${timeDif.minutes}分${timeDif.seconds}秒`
 }, 1000);
+
+
+
+function loadWisdom() {
+  fetch('https://goquotes-api.herokuapp.com/api/v1/random/1?type=tag&val=motivational')
+    .then(response => {
+      response.json()
+              .then((data) => {
+                const author = data.quotes[0].author;
+                const quote = data.quotes[0].text;
+                document.querySelector('#wisdom').innerHTML = `${quote} <br/><br/> ${author}`
+              });
+    })
+}
